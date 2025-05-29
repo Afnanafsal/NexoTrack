ff5194e026bf7bc519d8b78b50d55c8284c4f2b4
afnanafsal <afnanafsal004@gmail.com>
2025-05-29 23:27:09 +0530

    Remove debug banner from the app for a cleaner UI

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'NexoTrack',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
